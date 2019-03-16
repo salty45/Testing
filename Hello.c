9b87e47ecac57e0f21cf57ef6e0337ab4e42cdca
Sarah Larkin <selarkin@mtu.edu>
2019-03-15 23:03:31 -0400

    first commit

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * @author Sarah Larkin
+ * Testing github private repo with a hello world
+ * Date: 03/15/2019
+ */
+
+int main(void)
+{
+    printf("hello world!\n");
+    return 0;
+}
